64d6bd883d80cee13bd4f075cb36c762ece2a353
EloiseRosen <eloise.rosen@gmail.com>
2023-06-05 23:08:16 -0400

    add state and updating color to Tile component

--- src/Tile.js
+++ src/Tile.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const stateToColor = ['orange', 'black', 'red', 'pink', 'green'] // state corresponds to index (e.g. state 0 is orange)
 
 
 function Tile() {
+  const [state, setState] = useState(0);
+
   return (
-    <path d="M812,514 L878,311 L705,437 L640,640 Z" fill="pink"/>
+    <path 
+      d="M812,514 L878,311 L705,437 L640,640 Z" 
+      fill={stateToColor[state]} 
+      onClick={() => setState((prevState) => prevState === 4 ? 0 : prevState+1)}
+    />
   );
 }
 
